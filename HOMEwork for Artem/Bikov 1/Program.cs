31ae612d21e1f027ee7c6c76629e430d917d6d40
Artem Brilev <artem.brilev@ticos-systems.ch>
2018-11-16 19:12:08 +0300

    Artem  is  a MASTER

---
+++ HOMEwork for Artem/Bikov 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bikov_1
+{
+    class Program
+    {
+        static bool f(int x, int y)
+        {
+            if ((x % y == 0) || (y % x == 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            bool answer = f(a, b);
+            Console.WriteLine(answer);
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Artem is a MASTER");
+            Console.Read();
+        }
+    }
+}
---
+++ HOMEwork for Artem/artem lox 46/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace artem_lox_46
+{
+    class Program
+    {
+        static int f(int x)
+        {
+            int a = x;
+            int b = 0;
+            while (a != 0)
+            {
+
+                b = a % 10 + b * 10;
+                a = a / 10;
+            }
+            return b;
+        }
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int answer = f(a);
+            Console.WriteLine(answer);
+
+
+
+
+
+
+
+
+            Console.WriteLine("Artem is a MASTER");
+            Console.Read();
+        }
+    }
+}
